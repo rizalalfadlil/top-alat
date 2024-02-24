0c31d9f45b992d3b036b63f1f5883d55aaab87fb
RizalAlfadlil <rizalalfadlil@gmail.com>
2024-02-24 12:58:25 +0700

    edit-package

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     <>
     <head>
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Teko:wght@300..700&display=swap" rel="stylesheet"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Teko:wght@300..700&display=swap" rel="stylesheet"/>
     </head>
     <div className="App">
       <ListFl/>
